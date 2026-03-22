d51e9121bc84b15bfcae9f71a5f556ed174b67d6
Kyle G <gordokyl@mail.gvsu.edu>
2026-03-22 14:53:36 -0400

    Updated Beverage to Assignment 3

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -2,20 +2,26 @@
   <Mug>
     <Cold v-if="isIced" />
     <Hot v-else />
+
     <Contents>
       <template v-slot:top>
-        <Creamer />
+        <Creamer v-if="hasCreamer" :creamer="creamer" />
       </template>
+
       <template v-slot:mid>
-        <Syrup />
+        <!-- Only show syrup if it exists -->
+        <Syrup v-if="hasSyrup" :syrup="syrup" />
       </template>
+
       <template v-slot:bottom>
         <Base />
       </template>
     </Contents>
   </Mug>
 </template>
+
 <script setup lang="ts">
+import { computed } from "vue";
 import Contents from "./Contents.vue";
 import Mug from "./Mug.vue";
 import Syrup from "./Syrup.vue";
@@ -23,8 +29,15 @@ import Base from "./Base.vue";
 import Creamer from "./Creamer.vue";
 import Hot from "./Hot.vue";
 import Cold from "./Cold.vue";
+
 type Props = {
   isIced: boolean;
+  creamer: string;
+  syrup: string;
 };
-defineProps<Props>();
+
+const props = defineProps<Props>();
+
+const hasCreamer = computed(() => props.creamer !== "No Creamer");
+const hasSyrup = computed(() => props.syrup !== "No Syrup");
 </script>
