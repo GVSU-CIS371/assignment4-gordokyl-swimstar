9833ad75e01a673a7868788ea66577e40681eef7
Kyle G <gordokyl@mail.gvsu.edu>
2026-03-22 14:52:55 -0400

    Updated Base to Assignment 3

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,17 +1,42 @@
 <template>
-  <div class="baseBeverage"></div>
+  <div>
+    <div class="baseBeverage" :style="baseStyle"></div>
+
+    <select v-model="selectedBaseId">
+      <option v-for="b in bases" :key="b.id" :value="b.id">
+        {{ b.name }}
+      </option>
+    </select>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { bases } from "../stores/beverage";
+
+// store selected base by id (more reliable than storing objects)
+const selectedBaseId = ref(bases.value[0].id);
+
+// find the selected base object
+const selectedBase = computed(() =>
+  bases.value.find(b => b.id === selectedBaseId.value)
+);
+
+// dynamic style
+const baseStyle = computed(() => ({
+  backgroundColor: selectedBase.value?.color || "transparent"
+}));
+</script>
 
 <style scoped>
 .baseBeverage {
   position: relative;
-  width: 100%;
-  height: 100%;
+  width: 150px;
+  height: 200px;
   bottom: 0;
+  border: 2px solid black;
+  border-radius: 10px;
   animation: pour-tea 2s;
   z-index: 300;
-  /* // border-radius: 0.05em 0.05em 2.2em 2.2em; */
 }
 </style>
