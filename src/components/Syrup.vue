48552e6b5d9d468169a167e4d75a660bdd32d551
Kyle G <gordokyl@mail.gvsu.edu>
2026-03-22 14:55:00 -0400

    Updated Syrup to Assignment 3

--- src/components/Syrup.vue
+++ src/components/Syrup.vue
@@ -1,22 +1,42 @@
 <template>
-  <div class="syrup"></div>
+  <div>
+    <div class="baseSyrup" :style="baseStyle"></div>
+
+    <select v-model="selectedSyrupId">
+      <option v-for="s in syrups" :key="s.id" :value="s.id">
+        {{ s.name }}
+      </option>
+    </select>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { syrups } from "../stores/beverage";
+
+// store selected syrup by id (more reliable than storing objects)
+const selectedSyrupId = ref(syrups.value[0].id);
+
+// find the selected syrup object
+const selectedSyrup = computed(() =>
+  syrups.value.find(s => s.id === selectedSyrupId.value)
+);
+
+// dynamic style
+const baseStyle = computed(() => ({
+  backgroundColor: selectedSyrup.value?.color || "transparent"
+}));
+</script>
 
 <script setup lang="ts"></script>
 <style lang="scss" scoped>
 .syrup {
   transform: translateY(400%);
+  background-color: #c6c6c6;
   position: relative;
   width: 100%;
   height: 20%;
   animation: pour-tea 2s 1s forwards;
   z-index: 2;
-  background: repeating-linear-gradient(
-    45deg,
-    var(--texture-color),
-    var(--texture-color) 10px,
-    rgba(225, 207, 149, 1) 10px,
-    rgba(225, 207, 149, 1) 20px
-  );
 }
 </style>
